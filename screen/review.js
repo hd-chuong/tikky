021ea7c956baea357fdaa11f038a58800e987a98
Chuong <chuong.git@gmail.com>
2020-08-19 11:31:36 +1000

    connect database with new to-do-item

--- screen/review.js
+++ screen/review.js
@@ -1,8 +1,11 @@
 import React, {useState} from 'react';
 import {StyleSheet, View, Text, Button, FlatList, CheckBox} from 'react-native';
 import {globalStyles, images} from '../styles/global';
-import Card from '../components/card';
 import { TextInput } from 'react-native-gesture-handler';
+import update from 'react-addons-update';
+
+import * as SQLite from 'expo-sqlite';
+const db = SQLite.openDatabase("db.db");
 
 function displayTime(date)
 {
@@ -28,6 +31,7 @@ function displayTime(date)
     }
     else 
     {
+        // bug here
         const formatter = new Intl.DateTimeFormat('en', { month: 'short', day: 'numeric' });
         return "on " + formatter.format(date);
     }
@@ -38,13 +42,63 @@ export default class Review extends React.Component {
     {
         super(props);
         this.navigation = props.navigation;
+
+        this.pressHandler = this.pressHandler.bind(this);
+        this.addNewItem = this.addNewItem.bind(this);
+        this.load = this.load.bind(this);
+        this.updateItem = this.updateItem.bind(this);
+
         this.state = {
             item: this.navigation.getParam('item'),
             checklist: [],
-            newCheckbox: {hasDone: false, body: ""}
+            newCheckbox: {hasDone: 0, content: ""}
         }
-        this.pressHandler = this.pressHandler.bind(this);
-        this.addNewItem = this.addNewItem.bind(this);
+        this.load();
+
+    }
+    async updateItem(id, newData)
+    {
+        var targetIndex = -1;
+        for (let i = 0; i < this.state.checklist.length; ++i)
+        {
+            let todoItem = this.state.checklist[i];
+            if (todoItem.id == id)
+            {
+                targetIndex = i;
+            }
+        }
+
+        if (targetIndex != -1)
+        {
+            newData.hasDone = (newData.hasDone === 0) ? 1: 0;
+            this.setState(update(this.state, {
+                checklist: {
+                    [targetIndex]: {
+                        $set: newData
+                    }
+                }
+            }));
+
+            db.transaction(
+                (tx) => {
+                    tx.executeSql("UPDATE checklist SET hasDone = ? WHERE id = ?;", 
+                    [newData.hasDone, id]);
+                    }, () => {console.log("fail modifying list"), () => {console.log("success")}}
+            );
+        }
+    }
+
+    async load()   
+    {
+        db.transaction(tx => {
+            tx.executeSql(
+                "create table if not exists checklist (id integer PRIMARY KEY not null, content text, createdTime int, hasDone int, review_id INT, FOREIGN KEY (review_id) REFERENCES reviews (id));"
+            );
+
+            tx.executeSql("select * from checklist where review_id = ?", [this.state.item.id], (_, { rows: { _array, length } }) => {
+                this.setState({checklist: _array != null ? _array: []});
+            });
+        }, () => {console.log("Fail loading to do items")}, ()=> {console.log("Seems like successing in loading items")});
     }
 
     pressHandler()
@@ -56,8 +110,15 @@ export default class Review extends React.Component {
     
     addNewItem()
     {
-        let newItem = {...this.state.newCheckbox, id: Date.now()};
-        this.setState({checklist: [...this.state.checklist, newItem] });
+        let newCheckbox = {...this.state.newCheckbox, id: Date.now()};
+        this.setState({checklist: [...this.state.checklist, newCheckbox] });
+
+        db.transaction(tx => {
+            tx.executeSql(
+                "insert into checklist (id, content, createdTime, hasDone, review_id) values (?, ?, ?, ?, ?);",
+                [newCheckbox.id, newCheckbox.content, newCheckbox.id, newCheckbox.hasDone, this.state.item.id]
+            );
+        }, () => {console.log("Fail add new todo")}, () => {console.log("Seems like succeeding adding new todo")}  );
     }
 
     render() 
@@ -65,43 +126,51 @@ export default class Review extends React.Component {
         return (
             <View style={globalStyles.container}>
                     <View style={styles.titleLine}>
-                    <TextInput 
-                        multiline
-                        style={globalStyles.titleText} 
-                        defaultValue={this.state.item.title}
-                        onChangeText={(newTitle) => {this.setState({item: {...this.state.item, title: newTitle}})}}    
-                    /><Text>Created {displayTime(new Date(this.state.item.createdTime))}</Text>
+                        {/* Title */}
+                        <TextInput 
+                            multiline
+                            style={globalStyles.titleText} 
+                            defaultValue={this.state.item.title}
+                            onChangeText={(newTitle) => {this.setState({item: {...this.state.item, title: newTitle}})}}    
+                        />
+                        <Text>Created {displayTime(new Date(this.state.item.createdTime))}</Text>
                     </View>
-                    <TextInput 
-                        multiline 
-                        defaultValue={this.state.item.body} 
-                        onChangeText={(newBody) => {this.setState({item: {...this.state.item, body: newBody}})} } />
-    
+                        
+                        {/* Description */}
+                        <TextInput 
+                            multiline 
+                            defaultValue={this.state.item.body} 
+                            onChangeText={(newBody) => {this.setState({item: {...this.state.item, body: newBody}})} } 
+                        />
+
                     <View>
                         <FlatList
-                        data={this.state.checklist}
-                        renderItem={({item}) => {
-                            return (<View style={styles.checkboxLine}>
-                                        <CheckBox
-                                            value={item.hasDone}
-                                        /><Text>{item.body}</Text>
-                                    </View>
-                            )
-                        }}
+                            data={this.state.checklist}
+                            renderItem={({item, i}) => {
+                                return (<View style={styles.checkboxLine}>
+                                            <CheckBox
+                                                value={item.hasDone === 1}
+                                                onValueChange={() => {
+                                                    this.updateItem(item.id, item);
+                                                }}/><Text>{item.content}</Text>
+                                        </View>
+                                )
+                            }}
                         />
                         <View style={styles.checkboxLine}>
                             <CheckBox 
-                                value={this.state.newCheckbox.hasDone} 
+                                value={this.state.newCheckbox.hasDone === 1} 
                                 onValueChange={() => 
-                                        this.setState({
-                                                    hasDone: this.state.newCheckbox.hasDone, 
-                                                    body: this.state.newCheckbox.body})
+                                        this.setState({newCheckbox: {
+                                                    hasDone: this.state.newCheckbox.hasDone === 0 ? 1: 0, 
+                                                    content: this.state.newCheckbox.content
+                                                }})
                                 }/>
                             <TextInput 
                                 placeholder={"Enter an item here"} 
-                                defaultValue={this.state.newCheckbox.body}
-                                onChangeText={(body) => this.setState({newCheckbox: {hasDone: this.state.newCheckbox.hasDone, body}})}
-                                onSubmitEditing={()=> {this.addNewItem(); this.setState({newCheckbox: {hasDone: false, body: ""}})}}
+                                defaultValue={this.state.newCheckbox.content}
+                                onChangeText={(content) => this.setState({newCheckbox: {hasDone: this.state.newCheckbox.hasDone, content}})}
+                                onSubmitEditing={()=> {this.addNewItem(); this.setState({newCheckbox: {hasDone: false, content: ""}})}}
                             />
                         </View>
 
